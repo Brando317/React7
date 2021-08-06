dddb20d6c47f2a62fd83f0bb5fcb04ca8f966b21
Brando Collins <ghost.rkrv3@gmail.com>
2021-08-06 10:10:52 -0500

    Completion of Phase One

---
+++ src/array.js
@@ -0,0 +1,322 @@
+{
+    formula: "Cs",
+    name: "cesium",
+  },
+    {
+      formula: "F",
+    name: "fluorine",
+  },
+   {
+     formula: "Cl",
+   name: "chlorine", 
+  },
+    {
+      formula: "Ca",
+    name: "calcium",
+  },
+    {
+      formula: "U",
+  name: "uranium",
+  },
+  {
+  formula: "Xe",
+  name: "xenon",
+  },
+    {
+  formula: "Ne",
+  name: "neon",
+    },
+  {
+  formula: "Kr",
+  name: "krypton",
+  },
+  {
+    formula: "Pb",
+  name: "lead",
+  },
+  {
+    formula: "Co",
+    name: "cobalt",
+  },
+  {
+    formula: "Ra",
+  name: "radium",
+  },
+  {
+    formula: "Np",
+    name: "neptunium",
+    },  
+  {
+  formula: "Pu",
+  name: "plutonium",
+  },
+  {
+  formula: "Be",
+  name: "beryllium",
+  },
+  {
+    formula: "Fe",
+  name: "iron",
+  },
+  {
+    formula: "P",
+    name: "phosphrous",
+  },
+  {
+    formula: "Ni",
+  name: "nickel",
+  },
+  {
+    formula: "Tl",
+  name: "thallium",
+  },
+  {
+    formula: "Cd",
+  name: "cadmium",
+  },
+  {
+    formula: "Ar",
+  name: "argon",
+  },
+  {
+    formula: "nh4",
+  name: "amonium",
+  },
+    {
+  formula: "br",
+  name: "Bromine",
+  },
+  {
+  formula: "H2O",
+  name: "water",
+  },
+  {
+  formula: "NaCl",
+  name: "salt",
+  },
+  {
+  formula: "Al",
+  name: "Aluminum",
+  },
+  {
+  formula: "CO2",
+  name: "carbon dioxide",
+  },
+  {
+    formula: "c",
+    name: "carbon",
+    },  
+    {
+  formula: "o",    
+      name: "oxygen",
+    },
+      {
+    formula: "CO3",
+  name: "carbon trioxide",
+  },
+  {
+    formula: "C3H8",
+  name: "propane",
+  },
+  {
+    formula: "CO",
+  name: "carbon monoxide",
+  },
+  {
+    formula: "C12H22O11",
+  name: "sugar",
+  },
+  {
+  formula: "K",
+  name: "potassium",
+  },  
+  {
+  formula: "Na",
+  name: "sodium",
+  },
+  {
+    formula: "Au",
+  name: "gold",
+  },
+  {
+    formula: "Ag",
+    name: "silver",
+    },
+  {
+  formula: "H",
+  name: "hydrogen",
+  },
+  {
+    formula: "N",
+  name: "nitrogen",
+  },
+  {
+    formula: "He",
+  name: "helium",
+  },
+  {
+    formula: "C3H5N3O9",
+  name: "nitroglycerin",
+  },
+  {
+  formula: "c3 h6 n6 o6", 
+  name: "RDX",
+  },
+  {
+    formula: "CH3COOH",
+  name: "Acetic acid",
+  },
+  {
+  formula: "Al (OH)3",
+  name: "Aluminium hydroxide",
+  },
+  {
+    formula: "CH3COO-",
+    name: "acetate",
+  },
+  {
+    formula: "c3H6O",
+  name: "acetone",
+  },
+  {
+  formula: "C6H9AlO6",
+  name: "Aluminium acetate",
+  },
+  {
+  formula: "Al2 (CO3)3",
+  name: "aluminum carbonate",
+  },
+  {
+  formula: "AlCl3",
+  name: "Aluminum chloride",
+  },
+  {
+  formula: "AlF3",
+  name: "aluminum fluoride",
+  },
+  {
+  formula: "Ali3",
+  name:"aluminum iodide",
+  },
+  {
+  formula: "Al2 O3",
+  name: "aluminum oxide",
+  },
+  {
+  formula: "AlPO4",
+  name: "aluminum phosphate",
+  },
+  {
+  formula: "H2NCHRCOOH",
+  name: "amino acid",
+  },
+  {
+    formula: "Cr2H8N2O7",
+    name: "ammonium dichromate",
+  },
+  {
+  formula: "C2H3O2NH4",
+  name: "ammonium acetate",
+  },
+  {
+  formula: "NH4HCO3",
+  name: "ammonium bicarbonate",
+  },
+  {
+  formula: "NH4 BR",
+  name: "ammonium bromide",
+  },
+  {
+    formula: "(NH4)2 CO3",
+  name: "ammonium carbonate",
+  },
+  {
+  formula: "NH4 CL",
+  name: "ammonium chloride",
+  },
+  {
+  formula: "NH4, OH",
+  name: "ammonium hydroxide",
+  },
+  {
+    formula: "Po",
+  name: "polonium",
+  },
+  {
+    formula: "NH4 NO3",
+  name: "ammonium nitrate",
+  },
+  {
+  formula: "Al2 S3",
+  name: "aluminium sulfide",
+  },
+  {
+    formula: "NH4 NO2",
+  name: "ammonium nitrite",
+  },
+  {
+    formula: "NH4 2O",
+  name: "ammonium oxide",
+  },
+  {
+  formula: "(NH4)3 PO4",
+  name: "ammonium phosphate",
+  },
+  {
+  formula: "Ac",
+  name: "actinium",
+  },
+  {
+    formula: "Rn",
+  name: "radon",
+  },
+  {
+    formula: "Y",
+  name: "yttrium",
+  },
+  {
+   formula: "At",
+  name: "astatine",
+  },
+  {
+    formula: "Bi",
+  name: "bismuth",
+  },
+  {formula: "W",
+  name: "tungsten",
+  },
+  {
+    formula: "Sr",
+  name: "strontium",
+  },
+  {
+    formula: "Rb",
+  name: "rubidium",
+  },
+  {
+    formula: "Th",
+    name: "thorium",
+  },
+  {
+    formula: "Nd",
+    name: "neodymium",
+  },
+  {
+    formula: "Nb",
+    name: "niobium",
+  },
+  {
+    formula: "C6H8O6",
+  name: "Ascorbic acid",
+  },
+  {
+    formula: "Ba(C2H3O2)2",
+  name: "barium acetate",
+  },
+  {
+    formula: "BaCl2",
+  name: "Barium chloride",
+  },
+    ]
+    
+  
